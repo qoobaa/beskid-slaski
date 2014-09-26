dc01c1a4a09efcdbb4e459c486267697bd22820b
Kuba Kuźma <kuba@jah.pl>
2014-09-26 13:50:44 +0200

    using mobile angular ui

---
+++ www/js/application.js
@@ -0,0 +1,8 @@
+angular.module("BeskidSlaski", [
+    "mobile-angular-ui.fastclick",
+    "mobile-angular-ui.directives.toggle",
+    "mobile-angular-ui.directives.sidebars",
+    "BeskidSlaski.directives",
+]);
+
+angular.module("BeskidSlaski.directives", []);
---
+++ www/js/directives/leaflet.js
@@ -0,0 +1,93 @@
+angular.module("BeskidSlaski.directives")
+
+    .directive("bsLeaflet", function () {
+        return {
+            restrict: "E",
+            link: function (scope, element, attributes) {
+                var map, marker, base, peaks, shelters, paths, button, attribution, scale, zoom;
+
+                function toggleMenu() {
+                    scope.toggle("menu");
+                }
+
+                map = L.map(element[0], {
+                    center: L.latLng(49.707493, 19.013193),
+                    zoom: 11,
+                    minZoom: 11,
+                    maxZoom: 14,
+                    maxBounds: L.latLngBounds(L.latLng(49.3847, 18.7334), L.latLng(49.8013, 19.2443)),
+                    zoomControl: false,
+                    attributionControl: false
+                });
+
+                base = L.tileLayer("img/tiles/{z}/{x}/{y}.png");
+
+                marker = L.marker([0, 0], { icon: L.icon({ iconUrl: "img/marker.png", iconSize: [16, 16], iconAnchor: [8, 8] }) });
+
+                peaks = L.geoJson.ajax("geo/peaks.json", {
+ 	            pointToLayer: function (feature, latLng) {
+                        var icon = L.icon({ iconUrl: "img/peak.png", iconSize: [32, 32], iconAnchor: [16, 16] }),
+                            layer = L.marker(latLng, { icon: icon });
+
+                        layer.bindPopup(feature.properties.name);
+
+                        return layer;
+                    }
+                });
+
+                shelters = L.geoJson.ajax("geo/shelters.json", {
+ 	            pointToLayer: function (feature, latLng) {
+                        var icon = L.icon({ iconUrl: "img/shelter.png", iconSize: [32, 32], iconAnchor: [16, 16] }),
+                            layer = L.marker(latLng, { icon: icon });
+
+                        layer.bindPopup(feature.properties.name);
+
+                        return layer;
+                    }
+                });
+
+                paths = L.geoJson.ajax("geo/paths.json", {
+                    onEachFeature: function (feature, layer) {
+                        var popup = "<br>"
+                                + feature.properties.name + "<br>"
+                                + "Czas przejścia: " + feature.properties.times + "<br>"
+                                + "Dystans: " + feature.properties.distance + " km";
+
+                        feature.properties.colors.forEach(function (color) {
+                            popup = ""
+                                + "<div class='path'><div style='background: " + color + ";'></div></div>"
+                                + popup;
+                        });
+
+                        layer.setStyle({
+                            opacity: 1,
+                            color: feature.properties.colors[0]
+                        });
+
+                        feature.properties.colors.slice(1).forEach(function (color, i) {
+                            var additionalPath = L.polyline(layer.getLatLngs(), {
+                                opacity: 1,
+                                color: color,
+                                className: "path-" + (i + 1)
+                            });
+
+                            additionalPath.bindPopup(popup);
+                            paths.addLayer(additionalPath);
+                        });
+
+                        layer.bindPopup(popup);
+                    }
+                });
+
+                base.addTo(map);
+                // shelters.addTo(map);
+                // peaks.addTo(map);
+                // paths.addTo(map);
+
+                button = L.easyButton("fa-bars", toggleMenu, "Toggle Menu", map);
+                attribution = L.control.attribution({ prefix: false, position: "bottomleft" }).addAttribution('&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors').addTo(map);
+                scale = L.control.scale({ imperial: false, position: "bottomright" }).addTo(map);
+                zoom = L.control.zoom({ position: "topright" }).addTo(map);
+            }
+        };
+    });
